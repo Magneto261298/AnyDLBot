fcba73b72d179a8f6f74632e45cc14d517b68f04
Magneto261298 <60148557+Magneto261298@users.noreply.github.com>
2020-02-18 20:27:40 +0530

    Update get_external_link.py

--- plugins/get_external_link.py
+++ plugins/get_external_link.py
@@ -97,7 +97,6 @@ async def get_link(bot, update):
         await bot.edit_message_text(
             chat_id=update.chat.id,
             text="https://morning-voice-c5a7.magneto.workers.dev/0thers/{}.{}".format(str(update.from_user.id), str(download_extension))
-            disable_web_page_preview=True
         )
         try:
             os.remove(after_download_file_name)
